c5f2090804ba54e402809fbf7ed2ddd66b1c775d
John Else <john.else@citrix.com>
2014-03-18 09:21:46 +0000

    Add Irc_result to docs
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/irc_result.mli
+++ lib/irc_result.mli
@@ -1,4 +1,7 @@
+(** Handling of results from operations which may fail. *)
+
 type ('a, 'b) t = [
   | `Ok of 'a
   | `Error of 'b
 ]
+(** A generic type representing the result of an operation which may fail. *)
